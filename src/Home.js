50d9e51b00df947df2a33de88c0a59f0e4703659
Stephane Godin <pub.sg@free.fr>
2024-02-11 00:19:59 +0100

    update title

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     return (
     <>
     <div className="Home">
-      <h1>Desktop BrailleRAP</h1>
+      <h1>DesktopBrailleRAP</h1>
       
       <img src="./braillerap_logo.svg" width='50%' alt="BrailleRAP logo"/>
       </div>
