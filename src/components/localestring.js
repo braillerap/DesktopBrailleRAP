8f396bdf788430853b4b1f3db06483b4a2fc533e
Stephane Godin <pub.sg@free.fr>
2024-07-04 15:08:31 +0200

    fix locale id fallback

--- src/components/localestring.js
+++ src/components/localestring.js
@@ -1,48 +1,41 @@
 import locales from './locales.js';
-class LocaleString
-{
-    constructor ()
-    {
+class LocaleString {
+    constructor() {
         this.locale = locales;
-        this.selectedlocale = 'fr'
-        console.log ("LocaleString constructor");
-        
+        this.selectedlocale = 'fr'; // select fr as default
     }
-    getLocaleString (id)
-    {
-        
-        if(id in this.locale[this.selectedlocale].data)
+    getLocaleString(id) {
+
+        if (id in this.locale[this.selectedlocale].data)
             return (this.locale[this.selectedlocale].data[id])
-        console.log ("invalid id " + id + " locale " + this.selectedlocale)
-        
-        return ("Unknown locale string ???")
+        console.log("invalid id " + id + " locale " + this.selectedlocale)
+
+        //fallback to en locale
+        if (id in this.locale["en"].data)
+            return (this.locale["en"].data[id])
+        // definetly bad id
+        return ("Unknown locale id ???")
     }
-    getLocaleDir (dir)
-    {
+    getLocaleDir(dir) {
         return (this.locale[this.selectedlocale].dir)
     }
-    getBrailleReverse()
-    {
+    getBrailleReverse() {
         return (this.locale[this.selectedlocale].reverse)
     }
-    setLocaleCode (locale)
-    {
+    setLocaleCode(locale) {
         if (locale in this.locale)
             this.selectedlocale = locale;
-        
+
     }
-    getLocaleCode ()
-    {
+    getLocaleCode() {
         return (this.selectedlocale)
     }
-    getLocaleList ()
-    {
+    getLocaleList() {
         let list = [];
-        for (let locale in this.locale)
-            {
-                list.push (this.locale[locale]);
-                
-            }
+        for (let locale in this.locale) {
+            list.push(this.locale[locale]);
+
+        }
         return (list);
     }
 };
