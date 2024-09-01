e5bcc7f8355aeefb150a68af6b6b9be519a5bebb
crocsg <pub.sg@free.fr>
2024-09-01 16:47:44 +0200

    clean html search path

--- desktopbraillerap.py
+++ desktopbraillerap.py
@@ -343,17 +343,9 @@ def get_entrypoint():
         print(os.path.join(os.path.dirname(__file__), path))
         return os.path.exists(os.path.join(os.path.dirname(__file__), path))
 
-    if exists("../gui/index.html"):  # unfrozen development
-        return "../gui/index.html"
-
+   
     if exists("./build/index.html"):  # unfrozen development
         return "./build/index.html"
-
-    if exists("../Resources/gui/index.html"):  # frozen py2app
-        return "../Resources/gui/index.html"
-
-    if exists("./gui/index.html"):
-        return "./gui/index.html"
 
     raise Exception("No index.html found")
 
