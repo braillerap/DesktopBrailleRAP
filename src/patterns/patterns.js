8b7b2c13650f9788256c79baa1a1f82be4329bf3
Stephane Godin <pub.sg@free.fr>
2024-10-14 00:51:42 +0200

    pattern test

---
+++ src/patterns/patterns.js
@@ -0,0 +1,11 @@
+import patternsvg from './packagesvg.json'
+/*
+const patterns = {
+    
+    "hrz6":{fname:hpattern_6, data:hpattern_6} ,
+    "vert6":{fname:vpattern_6,data:""} ,
+    "zz6":{fname:zpattern_6,data:""} 
+}
+*/
+const patterns =patternsvg;
+export default patterns;
