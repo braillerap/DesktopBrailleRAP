25223ccf888c618ada87b7ff84aa33a856aab940
Stephane Godin <pub.sg@free.fr>
2024-12-08 19:25:56 +0100

    add Italian and Portuguese locales

--- src/components/locales.js
+++ src/components/locales.js
@@ -7,15 +7,19 @@ import Arabic from '../locales/ar.json';
 import Ukrainian from '../locales/uk.json';
 import SimpleChineese from '../locales/zh_hans.json';
 import Greek from '../locales/el.json'
+import Italian from '../locales/it.json';
+import Portuguese from '../locales/pt.json';
+
 const locales = {
     "ar":       {lang:"ar", dir:"rtl", desc:"ar -  عربي",               reverse:true, data:Arabic},
+    "de":       {lang:"de", dir:'ltr', desc:'de - Deutch',              reverse:false, data:Deutch},
     "es":       {lang:"es", dir:'ltr', desc:'es - Español',             reverse:false, data:Spanish},
     "el":       {lang:"el", dir:'ltr', desc:'el - ελληνικά',            reverse:false, data:Greek},
-    "de":       {lang:"de", dir:'ltr', desc:'de - Deutch',              reverse:false, data:Deutch},
-    "nl":       {lang:"nl", dir:'ltr', desc:'nl - Dutch',               reverse:false, data:Netherland},
-    
     "en":       {lang:"en", dir:"ltr", desc:"en - English",             reverse:false, data:English},
     "fr":       {lang:"fr", dir:'ltr', desc:'fr - Français',            reverse:false, data:French},
+    "it":       {lang:"it", dir:'ltr', desc:'it - Italiano',            reverse:false, data:Italian},
+    "nl":       {lang:"nl", dir:'ltr', desc:'nl - Dutch',               reverse:false, data:Netherland},
+    "pt":       {lang:"pt", dir:'ltr', desc:'pt - Português',           reverse:false, data:Portuguese},
     "uk":       {lang:"uk", dir:'ltr', desc:'uk - українська',          reverse:false, data:Ukrainian},
     "zh-hans":  {lang:"zh-hans", dir:'ltr', desc:'zh-hans - 简体中文',   reverse:false, data:SimpleChineese}
 }; 
