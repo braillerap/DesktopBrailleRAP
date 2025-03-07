af89b1d173f9f3ba502dbd49673c306f7fe3d42c
Stephane Godin <pub.sg@free.fr>
2025-03-07 22:37:13 +0100

    typo

--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -79,7 +79,7 @@ extensions = [
 	'sphinx.ext.autosectionlabel',
     'sphinx_rtd_theme',
     'myst_parser'
-]
+] 
 myst_enable_extensions = ["deflist", "attrs_inline"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
