aaad27d28638593333216116eb5910e548a7af9a
Stephane Godin <pub.sg@free.fr>
2025-03-08 00:08:41 +0100

    typo

--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -134,7 +134,7 @@ branch_to_theme_mapping = {
     'pyramid': 'pyramid',
     'bizstyle': 'bizstyle',
 }
-current_branch = None #get_git_branch()
+current_branch = get_git_branch()
 
 if current_branch:
     sphinx_html_theme = branch_to_theme_mapping.get(current_branch, default_html_theme)
@@ -153,7 +153,10 @@ html_theme = sphinx_html_theme
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
