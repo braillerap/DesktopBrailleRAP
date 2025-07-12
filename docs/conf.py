d4f2311b420081189c1da583f83f9ddbb2ed37d4
Stephane <pub.sg@free.fr>
2025-07-12 19:06:49 +0800

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,8 @@ master_doc = 'index'
 project = u'DesktopBrailleRAP'
 copyright = 'GPL V3'
 
-version = '0.8.1'
-release = '0.8.1'
+version = '0.8.4'
+release = '0.8.4'
 exclude_patterns = ['_build']
 default_role = 'obj'
 pygments_style = 'sphinx'
--- docs/conf_rtd.py
+++ docs/conf_rtd.py
@@ -59,9 +59,9 @@ copyright = u'GPL V3'
 author = u'BrailleRAP team'
 
 # The short X.Y version
-version = u'0.8.1'
+version = u'0.8.4'
 # The full version, including alpha/beta/rc tags
-release = u'0.8.1'
+release = u'0.8.4'
 
 
 # -- General configuration ---------------------------------------------------
