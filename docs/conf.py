5a1eaa1eb795c98aef9aaa80dc90c144e2381e9c
Stephane Godin <pub.sg@free.fr>
2026-05-03 22:51:22 +0200

    doc: merge old configuration file

--- docs/conf.py
+++ docs/conf.py
@@ -80,6 +80,117 @@ html_theme_options = {
     'display_version': True,
 }
 
+###########################################################
+# moved from conf_rtd
+###########################################################
+import os.path
+import re
+import subprocess
+import logging
+
+def get_git_branch():
+    """Get the git branch this repository is currently on"""
+    path_to_here = os.path.abspath(os.path.dirname(__file__))
+
+    # Invoke git to get the current branch which we use to get the theme
+    try:
+        p = subprocess.Popen(['git', 'branch'], stdout=subprocess.PIPE, cwd=path_to_here)
+
+        # This will contain something like "* (HEAD detached at origin/MYBRANCH)"
+        # or something like "* MYBRANCH"
+        branch_output = p.communicate()[0]
+
+        # This is if git is in a normal branch state
+        match = re.search(r'\* (?P<branch_name>[^\(\)\n ]+)', branch_output)
+        if match:
+            return match.groupdict()['branch_name']
+
+        # git is in a detached HEAD state
+        match = re.search(r'\(HEAD detached at origin/(?P<branch_name>[^\)]+)\)', branch_output)
+        if match:
+            return match.groupdict()['branch_name']
+    except Exception:
+        logger.exception(u'Could not get the branch')
+
+    # Couldn't figure out the branch probably due to an error
+    return None
+
+
+
+# Maps git branches to Sphinx themes
+default_html_theme = 'sphinx_rtd_theme'
+branch_to_theme_mapping = {
+    # 3rd party themes
+    'master': default_html_theme,
+    'doc': 'alabaster',
+    
+    # Sphinx built-in themes
+    'alabaster': 'alabaster',
+    'classic': 'classic',
+    'sphinxdoc': 'sphinxdoc',
+    'scrolls': 'scrolls',
+    'agogo': 'agogo',
+    'traditional': 'traditional',
+    'nature': 'nature',
+    'haiku': 'haiku',
+    'pyramid': 'pyramid',
+    'bizstyle': 'bizstyle',
+}
+current_branch = get_git_branch()
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    'papersize': 'a4paper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    'pointsize': '12pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'DesktopBrailleRAP.tex', project,
+     u'BrailleRAP team', 'manual'),
+]
+
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'BrailleRAPman', project,
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output ----------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'DesktopBrailleRAPtexinfo', project,
+     author, 'DesktopBrailleRAPtexinfo', 'Page composition software for Open source DIY Braille embosser BrailleRAP.',
+     'Miscellaneous'),
+]
+
+###########################################################
+# end moved from conf_rtd
+###########################################################
 
 def setup(app):
     #app.add_stylesheet('css/custom.css')
