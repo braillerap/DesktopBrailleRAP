e5e963ac7a2b937bfd82655a6a0ef562b2708253
Stephane <pub.sg@free.fr>
2026-05-16 22:49:26 +0200

    chore: update version

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,8 @@ master_doc = 'index'
 project = u'DesktopBrailleRAP'
 copyright = 'GPL V3'
 
-version = '0.9.0'
-release = '0.9.0'
+version = '0.10.0'
+release = '0.10.0'
 exclude_patterns = ['_build']
 default_role = 'obj'
 pygments_style = 'sphinx'
