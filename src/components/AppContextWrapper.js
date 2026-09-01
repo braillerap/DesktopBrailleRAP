d7e6b663b7005019c541a4128b8b6b0478a613f3
Stephane <pub.sg@free.fr>
2026-09-01 07:53:34 +0200

    fix: parameters backend call

--- src/components/AppContextWrapper.js
+++ src/components/AppContextWrapper.js
@@ -77,13 +77,8 @@ const AppContextWrapper = (props) => {
     }
     function setOption(opt) {
         setParams(opt);
-        console.log ("option receive in app context");
-        if (this.backend)
-        {
-            console.log ("send option to backend", opt);
-            backend.gcode_set_parameters(opt);
-        }
-
+        console.log ("option receive in app context");           
+        backend.gcode_set_parameters(opt);
     }
 
     function getLocaleString(id) {
